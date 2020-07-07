e3e22983a5a75d83b3094ba7cc66680c5eeb6f3e
kauvero <kavehrt60@gmail.com>
2020-07-06 17:34:05 -0700

    get request at endpoint /inventory is now getting all inventory items, this was tested with postman

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 app.use(express.json());
 app.use(cors());
+
+const inventory = require('./instock-data/inventory.json');
+
+app.get('/inventory', (req, res) => {
+    res.send(inventory);
+})
+
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
