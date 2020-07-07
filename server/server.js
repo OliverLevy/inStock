585958edebaacecaf51c10e8f054ae55a15fd28c
kauvero <kavehrt60@gmail.com>
2020-07-06 20:51:37 -0700

    router for inventory moved to routes folder and connected to server.js, postman test passed, .env files created , not used on my end as I am using the default 8080 port

--- server/server.js
+++ server/server.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const app = express();
-const cors = require("cors");
-app.use(express.json());
-app.use(cors());
 
-const inventory = require('./instock-data/inventory.json');
-
-app.get('/inventory', (req, res) => {
-    res.send(inventory);
-})
-
+let inventory_routes = require('./routes/inventory.js');
+app.use('/inventory', inventory_routes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
