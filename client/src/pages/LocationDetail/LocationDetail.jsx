8048dcf6163049a6d1a2ebf0fb811acb86a42594
Oliver Levy <oliver.levy.ol@gmail.com>
2020-07-07 19:43:52 -0400

    added functionality to the navbar. adding state to navbar to allow the underline to match the current page. OLLLAUALD-56

--- client/src/pages/LocationDetail/LocationDetail.jsx
+++ client/src/pages/LocationDetail/LocationDetail.jsx
@@ -4,7 +4,7 @@ import "./LocationDetail.scss";
 export default function LocationDetail() {
   return (
     <div>
-      <h1>this is the location detail</h1>
+      <h1>this is the location detail</h1> 
     </div>
   );
 }
