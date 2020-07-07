93c4f9a44e15e0774fa1e001ebde582868cfe244
Oliver Levy <oliver.levy.ol@gmail.com>
2020-07-07 19:16:44 -0400

    just pulled from development branch

--- server/routes/warehouse.js
+++ server/routes/warehouse.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const locations = require("../instock-data/locations.json");
+const inventory = require("../instock-data/inventory.json");
 
 // get ALL locations
 router.get("/", (req, res) => {
+  res.json(locations);
+});
+
+router.get("/:id", (req, res) => {
+  //filter through the inventory and return items that match the warehouse id
   res.json(locations);
 });
 
