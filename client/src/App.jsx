4c757e306282008a14e77103fa537e6a9aff98cc
Oliver Levy <oliver.levy.ol@gmail.com>
2020-07-06 22:33:11 -0400

    adding styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import InventoryList from './pages/InventoryList';
 import InventoryDetail from './pages/InventoryDetail';
 import LocationList from './pages/LocationList';
 import LocationDetail from './pages/LocationDetail';
+import NavBar from './components/NavBar/NavBar';
 import './App.scss';
 
 function App() {
   return (
     <div>
+      <NavBar />
       <InventoryList />
       <InventoryDetail />
       <LocationList />
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Logo from '../../assets/logo/Logo-instock.svg'
 import './NavBar.scss'
 
 export default function NavBar() {
   return (
-    <div>
-      
+    <div className='nav'>
+      <img src={Logo} alt=""/>
+      <h3>Inventory</h3>
+      <h3>Locations</h3>
     </div>
   )
 }
