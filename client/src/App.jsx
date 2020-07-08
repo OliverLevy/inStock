4953e9ba6887f9cf7ece7e7f71b2d25640fad071
Oliver Levy <oliver.levy.ol@gmail.com>
2020-07-08 11:38:11 -0400

    OLLLAUALD-56

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,30 @@
-import React from 'react';
-import InventoryList from './pages/InventoryList';
-import InventoryDetail from './pages/InventoryDetail';
-import LocationList from './pages/LocationList';
-import LocationDetail from './pages/LocationDetail';
-import { BrowserRouter, Route, Switch, Link} from "react-router-dom";
-import NavBar from './components/NavBar/NavBar';
-import './App.scss';
+import React from "react";
+import InventoryList from "./pages/InventoryList";
+import InventoryDetail from "./pages/InventoryDetail";
+import LocationList from "./pages/LocationList";
+import LocationDetail from "./pages/LocationDetail";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import "./App.scss";
 
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <NavBar />
 
-class App extends React.Component { 
-  render(){
-  return (
-    <div>
-      <BrowserRouter>
-      <NavBar />
-        <Link to='/warehouses/:id'> --Location Detail-warehouseDetails?-- </Link>
-        <Link to='/inventory/:id'> --Inventory Details--  </Link>
-        {/* <Link to='/inventory'> --All Inventory List--  </Link> */}
-        {/* <Link to='/warehouses'> --All Warehouses List--  </Link> */}
-        {/* <Link to='/'> --Location List-warehousesList?-- </Link> */}
-
-        <Switch>
-          <Route path='/warehouses/:id' component={LocationDetail} />
-          <Route path='/inventory/:id' component={InventoryDetail}/>
-          <Route path='/inventory'component={InventoryList} />
-          <Route path='/warehouses' component={LocationList} />
-          <Route path='/' exact component={LocationList} />
-        </Switch>
-
-      </BrowserRouter>
-    </div>
-  );
-}
+          <Switch>
+            <Route path="/warehouses/:id" component={LocationDetail} />
+            <Route path="/inventory/:id" component={InventoryDetail} />
+            <Route path="/inventory" component={InventoryList} />
+            <Route path="/warehouses" component={LocationList} />
+            <Route path="/" exact component={LocationList} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -3,26 +3,32 @@ import Logo from "../../assets/logo/Logo-instock.svg";
 import { Link } from "react-router-dom";
 import "./NavBar.scss";
 
-export default function NavBar() {
-  console.log("hi")
-  console.log()
-  return (
-    <div className="nav">
-      <Link to="/">
-        <img className="nav__logo" src={Logo} alt="InStock Logo" />
-      </Link>
-      <div className="nav__item-container">
-        <div className="nav__link-container">
-          <Link to="/inventory" className="nav__link--active">
-            <h3>Inventory</h3>
-          </Link>
-        </div>
-        <div className="nav__link-container">
-          <Link to="/warehouses" className="nav__link">
-            <h3>Locations</h3>
-          </Link>
+export default class NavBar extends React.Component {
+  render(){
+    console.log('hi')
+    console.log()
+    return (
+      <div className="nav">
+        <Link to="/">
+          <img className="nav__logo" src={Logo} alt="InStock Logo" />
+        </Link>
+        <div className="nav__item-container">
+          <div className="nav__link-container">
+            <Link to="/inventory" className="nav__link--active">
+              <h3>Inventory</h3>
+            </Link>
+          </div>
+          <div className="nav__link-container">
+            <Link to="/warehouses" className="nav__link">
+              <h3>Locations</h3>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+
+
+
