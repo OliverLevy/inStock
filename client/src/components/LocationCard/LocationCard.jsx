782ffa1998c7b6935eb6441fcca2eb9d909c3780
Dora He <kamiyajelly@gmail.com>
2020-07-08 00:28:20 -0400

    add axios call to get all the information for displaying warehouse list

--- client/src/components/LocationCard/LocationCard.jsx
+++ client/src/components/LocationCard/LocationCard.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
 import './LocationCard.scss'
 
-export default function LocationCard() {
+export default function LocationCard(props) {
+    const street = props.street;
+    const country = props.location;
+    const location = `${street}, ${country}`
     return (
         <section className="locationList__card locationCard">
         <section className="locationCard__group locationCard__group-warehouse">
           <section className="locationCard__warehouse">
-            <h3 className="locationCard__warehouse-name locationCard__txt">Warehouse Name Here</h3>
-            <p className="locationCard__warehouse-address locationCard__txt">warehouse location</p>
+    <h3 className="locationCard__warehouse-name locationCard__txt">{props.warehouseName}</h3>
+    <p className="locationCard__warehouse-address locationCard__txt">{location}</p>
           </section>
         </section>
 
         <section className="locationCard__group">
-          <p className="locationCard__txt">Mara Weinberg</p>
-        <p className="locationCard__txt locationCard__txt-job">Warehouse Manager</p>
+          <p className="locationCard__txt">{props.contactName}</p>
+    <p className="locationCard__txt locationCard__txt-job">{props.contactPosition}</p>
         </section>
 
         <section className="locationCard__group">
-        <p className='locationCard__txt locationCard__txt-phoneNumber'>+1 416 678 3254</p>
-        <p className="locationCard__txt locationCard__txt-email">weinberg@instack.com</p>
+        <p className='locationCard__txt locationCard__txt-phoneNumber'>{props.contactPhone}</p>
+        <p className="locationCard__txt locationCard__txt-email">{props.contactEmail}</p>
         </section>
 
         <section className="locationCard__group locationCard__group-description">
-        <p className="locationCard__txt locationCard__txt-description">Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation</p>
+        <p className="locationCard__txt locationCard__txt-description">{props.category}</p>
         </section>
 
         <section className="locationCard__arrowIcon">
--- client/src/pages/LocationList/LocationList.jsx
+++ client/src/pages/LocationList/LocationList.jsx
@@ -1,29 +1,62 @@
-import React, { Component } from 'react'
-import './LocationList.scss'
-import LocationCard from '../../components/LocationCard/LocationCard'
+import React, { Component } from "react";
+import "./LocationList.scss";
+import LocationCard from "../../components/LocationCard/LocationCard";
+import axios from "axios";
 
 export default class LocationList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      warehouse: [],
+    };
+  }
+
+  componentDidMount() {
+    console.log("this is did mount");
+    axios
+      .get(`http://localhost:8080/warehouses`)
+      .then((res) => {
+        this.setState({ warehouse: res.data });
+      })
+      .catch((error) => {
+        console.log("Warehouses Axios error");
+      });
+  }
+
   render() {
     return (
       <div className="locationList">
         <section className="locationList__header">
-        <h1>Locations</h1>
-        <input type="text" placeholder='Search' className='locationList__searchBar'/>
+          <h1>Locations</h1>
+          <input
+            type="text"
+            placeholder="Search"
+            className="locationList__searchBar"
+          />
         </section>
 
         <section className="locationList__labels">
-        <h5 className='locationCard__labels-horizontal'>WAREHOUSE</h5>
-        <h5 className='locationCard__labels-horizontal'>CONTACT</h5>
-        <h5 className='locationCard__labels-horizontal'>CONTACT INFORMATION</h5>
-        <h5 className='locationCard__labels-horizontal'>CATEGORIES</h5>
+          <h5 className="locationCard__labels-horizontal">WAREHOUSE</h5>
+          <h5 className="locationCard__labels-horizontal">CONTACT</h5>
+          <h5 className="locationCard__labels-horizontal">
+            CONTACT INFORMATION
+          </h5>
+          <h5 className="locationCard__labels-horizontal">CATEGORIES</h5>
         </section>
-
-        <LocationCard />
-
-        <LocationCard/>
-
+        {this.state.warehouse.map((item) => (
+          <LocationCard
+            key={item.id}
+            warehouseName={item.name}
+            street={item.address.street}
+            location={item.address.location}
+            contactName={item.contact.name}
+            contactPosition={item.contact.position}
+            contactEmail={item.contact.email}
+            contactPhone={item.contact.phone}
+            category={item.inventoryCategories}
+          />
+        ))}
       </div>
-    )
+    );
   }
 }
-
