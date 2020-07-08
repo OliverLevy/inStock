a465772a1c4d42d9920967705f426dd4c044b708
Dora He <kamiyajelly@gmail.com>
2020-07-08 16:12:40 -0400

    add a  new component "Dropdown" button for inventory card

---
+++ client/src/components/Dropdown/Dropdown.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import './Dropdown.scss';
+
+export default class Dropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false,
+    };
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener("click", this.turnOffMenu);
+    });
+  };
+
+  turnOffMenu = (event) => {
+      if(!this.dropdownMenu.contains(event.target)){
+
+          this.setState({ showMenu: false }, () => {
+            document.removeEventListener("click", this.turnOffMenu);
+          });
+      }
+  };
+
+  render() {
+    return (
+      <div className="dropdownMenu">
+        <button className="dropdownMenu__button" onClick={this.handleClick}>
+            
+        </button>
+        {this.state.showMenu ? (
+          <section className="dropdownMenu__container" ref={(element)=>{
+              this.dropdownMenu=element
+          }}>
+            <button className="dropdownMenu__remove-button">Remove</button>
+          </section>
+        ) : null}
+      </div>
+    );
+  }
+}
--- client/src/pages/InventoryList/InventoryList.jsx
+++ client/src/pages/InventoryList/InventoryList.jsx
@@ -1,8 +1,11 @@
 
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
 import InventoryCard from './components/InventoryCard';
 import './InventoryList.scss';
+import '../../components/Dropdown/Dropdown';
+import Dropdown from '../../components/Dropdown/Dropdown';
 
 
 
@@ -44,14 +47,18 @@ render() {
           <h5 className='inventoryCard__label-horizontal'>STATUS</h5>
 
         </section>
+        
+        <Dropdown />
 
         {this.state.inventory.map((item) => 
-
+        <Link key={item.id} to={`/inventory/${item.id}`}>
         <InventoryCard
           inventory = {this.state.inventory} key = {item.id} name={item.name} description={item.description} 
           quantity={item.quantity} lastOrdered={item.lastOrdered} city={item.city} isInstock={item.isInstock}
           category={item.category} warehouseId={item.warehouseId}/>
-        )}    
+        </Link>
+        )}
+          
     </div>
   );
 }}
