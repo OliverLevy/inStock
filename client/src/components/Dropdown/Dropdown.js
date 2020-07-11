edfdb7e84d4ce0826b44d0f067a91002b29c19e8
Dora He <kamiyajelly@gmail.com>
2020-07-11 14:57:01 -0400

    fix the error in warehouse.js, add a logical operator in turn off menu dropdown.js

--- client/src/components/Dropdown/Dropdown.js
+++ client/src/components/Dropdown/Dropdown.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import './Dropdown.scss';
+import "./Dropdown.scss";
 
 export default class Dropdown extends Component {
   constructor(props) {
@@ -8,39 +8,45 @@ export default class Dropdown extends Component {
       showMenu: false,
     };
   }
-  
+
   handleClick = (event) => {
     event.preventDefault();
     this.setState({ showMenu: true });
   };
 
   turnOffMenu = (event) => {
-    if(!this.dropdownMenu.contains(event.target)){
-
-        this.setState({ showMenu: false }, () => {
-          document.removeEventListener("click", this.turnOffMenu);
-        });
+    if (this.dropdownMenu && !this.dropdownMenu.contains(event.target)) {
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener("click", this.turnOffMenu);
+      });
     }
-};
+  };
 
-
-render() {
-  return (
-    <div className="dropdownMenu inventoryCard__removeIcon">
-      <button className="dropdownMenu__button" onClick={this.handleClick}>
-
-      </button>
-      {this.state.showMenu ? (
-        <section className="dropdownMenu__container" ref={(element)=>{
-            this.dropdownMenu=element
-        }}>
-          
-          <button className="dropdownMenu__remove-button" onClick={()=>{
-            this.props.handleRemove(this.props.inventoryId)
-          }}>Remove</button>
-        </section>
-      ) : null}
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="dropdownMenu inventoryCard__removeIcon">
+        <button
+          className="dropdownMenu__button"
+          onClick={this.handleClick}
+        ></button>
+        {this.state.showMenu ? (
+          <section
+            className="dropdownMenu__container"
+            ref={(element) => {
+              this.dropdownMenu = element;
+            }}
+          >
+            <button
+              className="dropdownMenu__remove-button"
+              onClick={() => {
+                this.props.handleRemove(this.props.inventoryId);
+              }}
+            >
+              Remove
+            </button>
+          </section>
+        ) : null}
+      </div>
+    );
+  }
 }
