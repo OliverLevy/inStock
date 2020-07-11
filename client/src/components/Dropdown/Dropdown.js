699cd6b3bfbc96e624bea2234c664cb9e1378cfc
Dora He <kamiyajelly@gmail.com>
2020-07-11 15:09:18 -0400

    fix the error in dropdown button component,

--- client/src/components/Dropdown/Dropdown.js
+++ client/src/components/Dropdown/Dropdown.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import "./Dropdown.scss";
-
+import './Dropdown.scss';
 export default class Dropdown extends Component {
   constructor(props) {
     super(props);
@@ -8,42 +7,34 @@ export default class Dropdown extends Component {
       showMenu: false,
     };
   }
-
   handleClick = (event) => {
     event.preventDefault();
-    this.setState({ showMenu: true });
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener("click", this.turnOffMenu);
+    });
   };
-
   turnOffMenu = (event) => {
-    if (this.dropdownMenu && !this.dropdownMenu.contains(event.target)) {
-      this.setState({ showMenu: false }, () => {
-        document.removeEventListener("click", this.turnOffMenu);
-      });
-    }
+    console.log(event)
+      if(this.dropdownMenu && !this.dropdownMenu.contains(event.target)){
+          this.setState({ showMenu: false }, () => {
+            document.removeEventListener("click", this.turnOffMenu);
+          });
+      }
   };
-
+//   this will prevent the page leading to inventory/id
+  refreshPage = ()=>{
+      window.location.reload(false)
+  }
   render() {
     return (
       <div className="dropdownMenu inventoryCard__removeIcon">
-        <button
-          className="dropdownMenu__button"
-          onClick={this.handleClick}
-        ></button>
+        <button className="dropdownMenu__button" onClick={this.handleClick}>
+        </button>
         {this.state.showMenu ? (
-          <section
-            className="dropdownMenu__container"
-            ref={(element) => {
-              this.dropdownMenu = element;
-            }}
-          >
-            <button
-              className="dropdownMenu__remove-button"
-              onClick={() => {
-                this.props.handleRemove(this.props.inventoryId);
-              }}
-            >
-              Remove
-            </button>
+          <section className="dropdownMenu__container" ref={(element)=>{
+              this.dropdownMenu=element
+          }}>
+            <button className="dropdownMenu__remove-button" >Remove</button>
           </section>
         ) : null}
       </div>
