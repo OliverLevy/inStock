8e422f79664bbf61cfd27ad12c95aa58bfa7d91b
kauvero <kavehrt60@gmail.com>
2020-07-10 23:09:21 -0700

    form added with six input fields and two buttons, submitting the form or clicking on the buttons will close the modal

--- client/src/pages/InventoryList/components/Modal/Modal.jsx
+++ client/src/pages/InventoryList/components/Modal/Modal.jsx
@@ -24,13 +24,31 @@ export default class AddInventory extends Component {
     render() {
         return (
             <div className="modal">
-                 <button src={addIcon} className="modal__button" onClick={this.handleOpenModal}></button>
+            <button src={addIcon} className="modal__button" onClick={this.handleOpenModal}></button>
                 <Modal className="modal__body"
+                    shouldCloseOnOverlayClick={false}
                     isOpen={this.state.showModal}
-                    contentLabel="Minimal Modal Example">
-                        <h1>this is modal</h1>
-                        <p>this is the body of the modal</p>
-                    <button className="modal__button" onClick={this.handleCloseModal}></button>
+                   >
+                        <h1>Create New</h1>
+                        <form onSubmit={this.handleCloseModal} action="">
+                            <label htmlFor="">
+                                <input type="text"/>
+                            </label>
+                            <label htmlFor="">
+                                <input type="text"/>
+                            </label>
+                            <label htmlFor="">
+                                <input type="text"/>
+                            </label>
+                            <label htmlFor="">
+                                <input type="text"/>
+                            </label>
+                            <label htmlFor="">
+                                <textarea type="text"/>
+                            </label>
+                            <button>SAVE</button>
+                            <button>CANCEL</button>
+                        </form>
                 </Modal>
             </div>
         )
