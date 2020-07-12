bf97786ee3fc89e70904856803ae2a7f499759df
Oliver Levy <oliver.levy.ol@gmail.com>
2020-07-11 21:55:21 -0400

    more styling added

--- client/src/pages/InventoryList/components/Modal/Modal.jsx
+++ client/src/pages/InventoryList/components/Modal/Modal.jsx
@@ -8,7 +8,8 @@ Modal.setAppElement("#root");
 
 class AddInventory extends React.Component {
   state = {
-    modelIsOpen: true,
+    modelIsOpen: false,
+    switchValue: false,
   };
 
   toggleModal = () => {
@@ -23,57 +24,102 @@ class AddInventory extends React.Component {
     }
   };
 
+  handleSwitch = () => {
+    if (this.state.switchValue === false) {
+      this.setState({
+        switchValue: true,
+      });
+    } else {
+      this.setState({
+        switchValue: false,
+      });
+    }
+  };
+
+  switchDisplayValue = () => {
+    if(this.state.switchValue === true){
+      return(
+        <p>In Stock</p>
+      )
+    } else {
+      return (
+        <p>Out of Stock</p>
+      )
+    }
+  }
+
   render() {
+    
     return (
       <div>
-        <button onClick={this.toggleModal}>Open Model</button>
+        <button onClick={this.toggleModal} className="add-item__open">
+          <img src={addIcon} alt="add item button" className="add-item__icon" />
+        </button>
         <Modal
           isOpen={this.state.modelIsOpen}
           className="add-item__modal"
           overlayClassName="add-item__modal-overlay"
-          // bodyOpenClassName="add-item__document-body"
+          bodyOpenClassName="add-item__document-body"
           htmlOpenClassName="add-item__document-body"
         >
           <h1>Create New</h1>
           <form className="add-item__form">
-            <label>
-              <h5 className="add-item__label">PRODUCT</h5>
-              <input type="text" className="add-item__input" />
-            </label>
-            <label>
-              <h5 className="add-item__label">LAST ORDERED</h5>
-              <input type="date" className="add-item__input" />
-            </label>
-
-            <label>
-              <h5 className="add-item__label">CITY</h5>
-              <input type="text" className="add-item__input" />
-            </label>
-            <label>
-              <h5 className="add-item__label">COUNTRY</h5>
-              <select className="add-item__input">
-                <option>Canada</option>
-              </select>
-            </label>
-
-            <label>
-              <h5 className="add-item__label">QUANTITY</h5>
-              <input type="number" className="add-item__input" />
-            </label>
-            <div>
-              <h5 className="add-item__label">status</h5>
-              <div className='add-item__in-stock-container'>
-                <p>In Stock</p>
-                <ToggleSwitch
-                  onChange={this.handleSwitch}
-                  checked={this.state.switchValue}
+            <div className="add-item__container">
+              <label className="add-item__input-container">
+                <h5 className="add-item__label">PRODUCT</h5>
+                <input
+                  type="text"
+                  className="add-item__input"
+                  placeholder="Item Name"
                 />
+              </label>
+              <label className="add-item__input-container">
+                <h5 className="add-item__label">LAST ORDERED</h5>
+                <input type="date" className="add-item__input" />
+              </label>
+
+              <label className="add-item__input-container">
+                <h5 className="add-item__label">CITY</h5>
+                <input
+                  type="text"
+                  className="add-item__input"
+                  placeholder="City"
+                />
+              </label>
+              <label className="add-item__input-container">
+                <h5 className="add-item__label">COUNTRY</h5>
+                <select className="add-item__input">
+                  <option>Canada</option>
+                </select>
+              </label>
+
+              <label className="add-item__input-container">
+                <h5 className="add-item__label">QUANTITY</h5>
+                <input
+                  type="number"
+                  className="add-item__input"
+                  placeholder="0"
+                />
+              </label>
+              <div className="add-item__input-container">
+                <h5 className="add-item__label">status</h5>
+                <div className="add-item__in-stock-container">
+                  {this.switchDisplayValue()}
+                  <ToggleSwitch
+                    onChange={this.handleSwitch}
+                    checked={this.state.switchValue}
+                  />
+                </div>
               </div>
             </div>
 
-            <label>
+            <label className="">
               <h5 className="add-item__label">ITEM DESCRIPTION</h5>
-              <textarea type="text" className="add-item__text-area" />
+              <textarea
+                type="text"
+                className="add-item__text-area"
+                placeholder="(Optional)"
+              />
             </label>
 
             <div className="add-item__btn-container">
